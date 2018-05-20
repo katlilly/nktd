320524aef05bff102f35c0cfe7f906dce1e86e8e
Unknown <davda941@student.otago.ac.nz>
2018-05-20 22:00:28 +1200

    Test activity saves dictionaries/models to phones storage now. Have to fiddle with permissions to make progress. References #11

--- app/src/main/java/nktd/TetrisActivity.java
+++ app/src/main/java/nktd/TetrisActivity.java
@@ -1,40 +1,101 @@
 package nktd;
 
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
 
 import app.R;
-import nktd.speech_recognition.Recognizer;
+import edu.cmu.pocketsphinx.Assets;
+import edu.cmu.pocketsphinx.Hypothesis;
+import edu.cmu.pocketsphinx.RecognitionListener;
+import edu.cmu.pocketsphinx.SpeechRecognizer;
+import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
 
-public class TetrisActivity extends AppCompatActivity {
+public class TetrisActivity extends AppCompatActivity implements RecognitionListener{
 
-    Recognizer recognizer;
-    boolean recognizerBound = false;
+    SpeechRecognizer recognizer;
+    String result;
+    boolean resultAchieved = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tetris);
-        Intent intent = new Intent(this, Recognizer.class);
-        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+        setupRecognizer();
     }
 
-    private ServiceConnection serviceConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            Recognizer.RecognizerBinder recognizerBinder = (Recognizer.RecognizerBinder) service;
-            recognizer = recognizerBinder.getService();
-            recognizerBound = true;
+    @Override
+    protected void onStart(){
+        super.onStart();
+        while(recognizer != null) {
+            Log.d("here", "we are");
+            Log.d("result", interpretCommand(recognizer));
         }
+    }
 
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            recognizerBound = false;
+    public void setupRecognizer(){
+        File assetsDir;
+        try {
+            Assets assets = new Assets(this);
+            assetsDir = assets.syncAssets();
+            Log.d("here","here");
+            this.recognizer = SpeechRecognizerSetup.defaultSetup()
+                    .setAcousticModel(new File(assetsDir
+                            , "cmusphinx-en-us-ptm-5.2"))
+                    .setDictionary(new File(assetsDir
+                            , "tetris-dictionary.dic"))
+                    .getRecognizer();
+            File grammarFile = new File(assetsDir, "tetris-grammar.gram");
+            recognizer.addGrammarSearch("tetris", grammarFile);
+        } catch (IOException ex) {
+            Log.e("AssetInit", ex.getMessage());
         }
-    };
+    }
+
+    public String interpretCommand(SpeechRecognizer recognizer){
+        this.result = null;
+        this.resultAchieved = false;
+        recognizer.startListening("tetris", 10000);
+        while(this.resultAchieved == false) {
+
+        }
+        return this.result;
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+
+    }
+
+    @Override
+    public void onPartialResult(Hypothesis hypothesis) {
+
+    }
+
+    @Override
+    public void onResult(Hypothesis hypothesis){
+        if (hypothesis == null) {
+            return;
+        }
+        this.result = hypothesis.getHypstr();
+        this.resultAchieved = true;
+    }
+
+    @Override
+    public void onError(Exception e) {
+
+    }
+
+    @Override
+    public void onTimeout() {
+
+    }
 }
