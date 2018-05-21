75580c3a51a50274d796a439240b0e08c8d4cba7
Unknown <davda941@student.otago.ac.nz>
2018-05-22 09:34:02 +1200

    Added recognizer setup. References #11

--- app/src/main/java/com/example/nathan/nktd/MainActivity.java
+++ app/src/main/java/com/example/nathan/nktd/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.nathan.nktd;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+
+import edu.cmu.pocketsphinx.Assets;
+import edu.cmu.pocketsphinx.Hypothesis;
+import edu.cmu.pocketsphinx.RecognitionListener;
+import edu.cmu.pocketsphinx.SpeechRecognizer;
+import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
+
+public class MainActivity extends AppCompatActivity implements RecognitionListener{
     public static final String EXTRA_MESSAGE = "com.example.nathan.nktd.MESSAGE";
+
+    private SpeechRecognizer recognizer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new SetupTask(this).execute();
     }
 
     public void openTetris(View view){
@@ -32,5 +47,68 @@ public class MainActivity extends AppCompatActivity {
     public void openG4(View view){
         Intent intent = new Intent(this, Game4Activity.class);
         startActivity(intent);
+    }
+
+    private void setupRecognizer(File assetsDir) throws IOException {
+
+        recognizer = SpeechRecognizerSetup.defaultSetup()
+                .setAcousticModel(new File(assetsDir, "cmusphinx-en-us-ptm-5.2"))
+                .setDictionary(new File(assetsDir, "tetris-dictionary.dic"))
+                .getRecognizer();
+        recognizer.addListener(this);
+
+        File tetrisGrammar = new File(assetsDir, "tetris-grammar.gram");
+        recognizer.addGrammarSearch("tetris", tetrisGrammar);
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+
+    }
+
+    @Override
+    public void onPartialResult(Hypothesis hypothesis) {
+
+    }
+
+    @Override
+    public void onResult(Hypothesis hypothesis) {
+
+    }
+
+    @Override
+    public void onError(Exception e) {
+
+    }
+
+    @Override
+    public void onTimeout() {
+
+    }
+
+
+    private static class SetupTask extends AsyncTask<Void, Void, Exception> {
+        WeakReference<MainActivity> activityReference;
+
+        SetupTask(MainActivity activity) {
+            this.activityReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        protected Exception doInBackground(Void... params) {
+            try {
+                Assets assets = new Assets(activityReference.get());
+                File assetDir = assets.syncAssets();
+                activityReference.get().setupRecognizer(assetDir);
+            } catch (IOException e) {
+                return e;
+            }
+            return null;
+        }
     }
 }
